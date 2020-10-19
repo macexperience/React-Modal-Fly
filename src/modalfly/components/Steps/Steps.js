6987140c93588ed3d23d8b890010b0bdc42319ee
Fernando Flores <fqflores@gmail.com>
2020-10-19 17:01:49 -0400

    REMOVED: unused react import

--- src/modalfly/components/Steps/Steps.js
+++ src/modalfly/components/Steps/Steps.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import { useContext, useState } from 'react';
 import StepContext from '../../hooks/StepContext';
 import * as types from '../../hooks/actions';
 
